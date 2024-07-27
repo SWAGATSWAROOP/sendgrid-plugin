6f3ec540774461f178f41d3a04cb1c77742938a1
SWAGAT SWAROOP PARIDA <swagatswaroop@gmail.com>
2024-07-27 22:22:40 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require("express");
+
+const app = express();
+
+const sgMail = require("@sendgrid/mail");
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+app.post("/send-email", async (req, res) => {
+  const { to, subject, text } = req.body;
+
+  if (!to || !subject || !text) {
+    return res
+      .status(400)
+      .send({ error: "Missing required fields: to, subject, text" });
+  }
+
+  try {
+    const htmlText = text.replace(/\n/g, "<br>");
+    const msg = {
+      to: to,
+      from: "on-demand <pranjal@schoolhack.ai>",
+      subject: subject,
+      text: text,
+      html: `${htmlText}`,
+    };
+    sgMail
+      .send(msg)
+      .then(() => {
+        res.json("Email sent");
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  } catch (error) {
+    res.status(500).send({
+      success: false,
+      message: "Error sending email",
+      error: error.message,
+    });
+  }
+});
+
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => {
+  `Server is running of port ${PORT}`;
+});
